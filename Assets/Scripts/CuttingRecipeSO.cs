829f4ece473b08e9fa836066bf64b471c33ef43a
Michael van der Nest <michaelvdnest@gmail.com>
2024-01-05 22:34:56 +0200

    Implement current recipes and only cut relevant ingredients

---
+++ Assets/Scripts/CuttingRecipeSO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class CuttingRecipeSO : ScriptableObject {
+    [SerializeField] public KitchenObjectSO input;
+    [SerializeField] public KitchenObjectSO output;
+}
