c34922d7b1e2ddf9aef5422b585ba014990863c9
Michael van der Nest <michaelvdnest@gmail.com>
2024-01-07 19:55:42 +0200

    Add a visual for the items on a plate

---
+++ Assets/Scripts/KitchenObjects/PlateCompleteVisual.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateCompleteVisuall : MonoBehaviour {
+
+
+    [Serializable]
+    public struct KitchenObjectSO_GameObject {
+        public KitchenObjectSO kitchenObjectSO;
+        public GameObject gameObject;
+    }
+
+    [SerializeField] PlateKitchenObject plateKitchenObject;
+    [SerializeField] List<KitchenObjectSO_GameObject> KitchenObjectSOGameObjectList;
+
+    private void Start() {
+        plateKitchenObject.OnIngredientAdded += PlateKitchenObject_OnIngredientAdded;
+        foreach (KitchenObjectSO_GameObject kitchenObjectSOGameObject in KitchenObjectSOGameObjectList) {
+            kitchenObjectSOGameObject.gameObject.SetActive(false);
+        }
+    }
+
+    private void PlateKitchenObject_OnIngredientAdded(object sender, PlateKitchenObject.OnIngredientAddedEventArgs e) {
+        foreach (KitchenObjectSO_GameObject kitchenObjectSOGameObject in KitchenObjectSOGameObjectList) {
+            if (kitchenObjectSOGameObject.kitchenObjectSO == e.kitchenObjectSO) {
+                kitchenObjectSOGameObject.gameObject.SetActive(true);
+            }
+        }
+    }
+}
