6666a13f0a5532066585f3b726707d9ee60c9516
Michael van der Nest <michaelvdnest@gmail.com>
2024-01-09 18:37:06 +0200

    Stop null error when hot loading

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -12,15 +12,23 @@ public class GameInput : MonoBehaviour
 
     PlayerInputActions playerInputActions;
 
-    private void Start() {
+    private void Awake() {
         playerInputActions = new PlayerInputActions();
+    }
+
+    private void Start() {
         playerInputActions.Player.Enable();
 
         playerInputActions.Player.Interact.performed += Interact_performed;
         playerInputActions.Player.InteractAlternate.performed += InteractAlternate_performed;
     }
 
-   public Vector2 GetMovementVectorNormalized() {
+    public Vector2 GetMovementVectorNormalized() {
+
+        if (playerInputActions == null) {
+            playerInputActions = new PlayerInputActions();
+            Start();
+        }
 
         Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
 
